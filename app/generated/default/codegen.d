40da4a4c4533ec2189cfaa0715be6cafa81a0ca1
genrogamer <genrogamer@gmail.com>
2026-09-20 21:48:57 -0300

    feat(build): add rexglue-sdk and generated sources for remote CI/CD compilation

---
+++ app/generated/default/codegen.d
@@ -0,0 +1,5 @@
+/media/windroid/SSD\ KING/NFSMW-Recompiled/app/generated/default/codegen.build.stamp: \
+  /media/windroid/SSD\ KING/NFSMW-Recompiled/app/../assets/game_root/default.xex \
+  /media/windroid/SSD\ KING/NFSMW-Recompiled/app/nfsmw_manifest.toml \
+  /media/windroid/SSD\ KING/NFSMW-Recompiled/app/huecos.toml \
+  /media/windroid/SSD\ KING/NFSMW-Recompiled/app/overrides.toml
